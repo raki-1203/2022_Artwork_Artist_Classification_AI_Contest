36880a8b9477e085cc0bff82d95c40aa128863fe
raki-1203 <jfhdzzang@gmail.com>
2022-11-03 10:34:42 +0900

    실험 명령어 정리

--- sh_file/experiment.sh
+++ sh_file/experiment.sh
@@ -15,5 +15,15 @@
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_resnext50_32x4d --predict_path ./predict/image_train_fq_valid_q_resnext50_32x4d
 
 # Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size
-python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 8 --accumulation_steps 4 --wandb --model_name_or_path vit_base_patch16_384 --output_path ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --scheduler get_cosine_schedule_with_warmup --loss WeightedCrossEntropy
-python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_base_patch16_384
+#python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 8 --accumulation_steps 4 --wandb --model_name_or_path vit_base_patch16_384 --output_path ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --scheduler get_cosine_schedule_with_warmup --loss WeightedCrossEntropy
+#python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_base_patch16_384
+#python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_base_patch16_384 --tta
+
+# Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size
+#python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 8 --accumulation_steps 4 --wandb --model_name_or_path vit_base_patch18_384 --output_path ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --loss WeightedCrossEntropy --lr 5e-5
+#python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384
+#python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --tta
+
+# Image baseline Train full size & Quarter size image vit_large_patch16_384 Train Validation Quarter size
+python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 2 --accumulation_steps 16 --wandb --lr 5e-5 --model_name_or_path vit_large_patch16_384 --output_path ./saved_model/image_train_fq_valid_q_vit_large_patch16_384 --loss WeightedCrossEntropy
+python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_large_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_large_patch16_384
