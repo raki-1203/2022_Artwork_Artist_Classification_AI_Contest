846dd6866dc7eaa9763b52feacf938b6cd431309
raki-1203 <jfhdzzang@gmail.com>
2022-11-03 10:39:38 +0900

    실험 명령어 & 성능 정리

--- sh_file/experiment.sh
+++ sh_file/experiment.sh
@@ -1,25 +1,25 @@
-# Image baseline Quarter size image resnext50_32x4d Train
+# Image baseline Quarter size image resnext50_32x4d Train -> 0.301354
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 32 --wandb --model_name_or_path resnext50_32x4d --output_path ./saved_model/image_resnext50_32x4d
 #python inference.py --device 1 --output_path_list ./saved_model/image_resnext50_32x4d --predict_path ./predict/image_resnext50_32x4d
 
-# Image baseline full size image resnext50_32x4d Train
+# Image baseline full size image resnext50_32x4d Train -> 0.558937
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 32 --wandb --model_name_or_path resnext50_32x4d --output_path ./saved_model/image_full_size_resnext50_32x4d
 #python inference.py --device 1 --output_path_list ./saved_model/image_full_size_resnext50_32x4d --predict_path ./predict/image_full_size_resnext50_32x4d
 
-# Image baseline full size image resnext50_32x4d Train Validation Quarter size
+# Image baseline full size image resnext50_32x4d Train Validation Quarter size -> 0.714959
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 32 --wandb --model_name_or_path resnext50_32x4d --output_path ./saved_model/image_full_size_valid_quarter_resnext50_32x4d
 #python inference.py --device 1 --output_path_list ./saved_model/image_full_size_valid_quarter_resnext50_32x4d --predict_path ./predict/image_full_size_valid_quarter_resnext50_32x4d
 
-# Image baseline Train full size & Quarter size image resnext50_32x4d Train Validation Quarter size
+# Image baseline Train full size & Quarter size image resnext50_32x4d Train Validation Quarter size -> 0.748545
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 32 --wandb --model_name_or_path resnext50_32x4d --output_path ./saved_model/image_train_fq_valid_q_resnext50_32x4d --scheduler get_cosine_schedule_with_warmup --loss WeightedCrossEntropy
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_resnext50_32x4d --predict_path ./predict/image_train_fq_valid_q_resnext50_32x4d
 
-# Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size
+# Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size -> 0.633569, 0.589128
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 8 --accumulation_steps 4 --wandb --model_name_or_path vit_base_patch16_384 --output_path ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --scheduler get_cosine_schedule_with_warmup --loss WeightedCrossEntropy
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_base_patch16_384
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_vit_base_patch16_384 --tta
 
-# Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size
+# Image baseline Train full size & Quarter size image vit_base_patch16_384 Train Validation Quarter size -> 0.724353, 0.696572
 #python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 8 --accumulation_steps 4 --wandb --model_name_or_path vit_base_patch18_384 --output_path ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --loss WeightedCrossEntropy --lr 5e-5
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384
 #python inference.py --device 1 --output_path_list ./saved_model/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --predict_path ./predict/image_train_fq_valid_q_lr5e-5_ReduceLR_vit_base_patch16_384 --tta
