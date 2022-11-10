8038122f856dfd70178c460000d6240c1cf55220
raki-1203 <jfhdzzang@gmail.com>
2022-11-10 23:05:40 +0900

    cutout 적용 efficientnet_b4

--- sh_file/experiment.sh
+++ sh_file/experiment.sh
@@ -63,13 +63,13 @@
 # Image baseline + model efficientnet_b4, resnext50_32x4d Ensemble Inference -> 0.860458
 #python inference.py --device 1 --output_path_list ./saved_model/image_baseline_efficientnet_b4 ./saved_model/image_baseline_resnext50_32x4d --predict_path ./predict/image_baseline_ensemble_efficientnet_b4_resnext50_32x4d_cv --tta
 
-# Image baseline + model convnext_tiny_384_in22ft1k Cross Validation ->
+# Image baseline + model convnext_tiny_384_in22ft1k Cross Validation -> 0.83268, 0.870126
 #python train.py --is_train --use_amp --device 1 --epochs 20 --train_batch_size 32 --accumulation_steps 1 --wandb --model_name_or_path convnext_tiny_384_in22ft1k --output_path ./saved_model/image_baseline_convnext_tiny_384_in22ft1k --loss WeightedCrossEntropy --beta 1 --cutmix_prob 0.5 --cv
 #python inference.py --device 1 --output_path_list ./saved_model/image_baseline_convnext_tiny_384_in22ft1k --predict_path ./predict/image_baseline_convnext_tiny_384_in22ft1k_cv --tta
 #python inference.py --device 1 --output_path_list ./saved_model/image_baseline_efficientnet_b4 ./saved_model/image_baseline_resnext50_32x4d ./saved_model/image_baseline_convnext_tiny_384_in22ft1k --predict_path ./predict/image_baseline_ensemble_efficientnet_b4_resnext50_32x4d_convnext_tiny_384_in22ft1k_cv --tta --ensemble
 
-# Image baseline + cutout + model efficientnet_b4 Cross Validation ->
-python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 16 --accumulation_steps 2 --wandb --model_name_or_path efficientnet_b4 --output_path ./saved_model/image_baseline_cutout_efficientnet_b4 --loss WeightedCrossEntropy --beta 1 --cutmix_prob 0.5 --cv
-python inference.py --device 1 --output_path_list ./saved_model/image_baseline_cutout_efficientnet_b4 --predict_path ./predict/image_baseline_cutout_efficientnet_b4_cv --tta
-python inference.py --device 1 --output_path_list ./saved_model/image_baseline_cutout_efficientnet_b4 --predict_path ./predict/image_baseline_ensemble_efficientnet_b4_resnext50_32x4d_convnext_tiny_384_in22ft1k_cutout_efficientnet_b4_cv --tta --ensemble
+# Image baseline + cutout + model efficientnet_b4 Cross Validation -> 0.8242523,
+#python train.py --is_train --use_amp --device 1 --epochs 30 --train_batch_size 16 --accumulation_steps 2 --wandb --model_name_or_path efficientnet_b4 --output_path ./saved_model/image_baseline_cutout_efficientnet_b4 --loss WeightedCrossEntropy --beta 1 --cutmix_prob 0.5 --cv
+#python inference.py --device 1 --output_path_list ./saved_model/image_baseline_cutout_efficientnet_b4 --predict_path ./predict/image_baseline_cutout_efficientnet_b4_cv --tta
+python inference.py --device 1 --output_path_list ./saved_model/image_baseline_efficientnet_b4 ./saved_model/image_baseline_resnext50_32x4d ./saved_model/image_baseline_convnext_tiny_384_in22ft1k ./saved_model/image_baseline_cutout_efficientnet_b4 --predict_path ./predict/image_baseline_ensemble_efficientnet_b4_resnext50_32x4d_convnext_tiny_384_in22ft1k_cutout_efficientnet_b4_cv --tta --ensemble
 
